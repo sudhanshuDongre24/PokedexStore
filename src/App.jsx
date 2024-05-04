51daa11636f7424c9449ce739be663cf64dcd37b
Sudhanshu <sudhanshuDongre24@gmail.com>
2024-05-04 18:04:15 +0530

    input field  added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,23 @@ import Header from "./components/Header";
 import Input from "./components/Input";
 
 function App() {
+  const logChange = (e) => {
+    console.log(e.target.value);
+  };
   return (
     <>
       <div className="bg-[rgb(29,29,29)] min-h-screen text-white">
-        <div className="relative">
-          <Header />
+        {/* <Header /> */}
+        <div className="flex justify-center items-center min-h-screen">
+          <div className="flex gap-8 flex-col">
+            <Input
+              label="Email"
+              type="email"
+              placeholder="Enter Email..."
+              className="size-[320px]"
+              onChange={logChange}
+            />
+          </div>
         </div>
       </div>
     </>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,50 @@
 import React from "react";
 
-function Input({ childre }) {
+/* 
+Input component 
+
+input value take 
+1. Label
+2. Placeholder
+3. type
+4. onChange
+5. className
+6. props
+ 
+input tags are smart they keep all elements size uniform. 
+
+*/
+
+function Input({
+  label = "",
+  placeholder = "",
+  type = "text",
+  onChange = "",
+  className = "",
+  ...props
+}) {
   return (
-    <>
-      <label>Sam</label>
-      <input type="text" className="" />
-    </>
+    <div className="flex flex-col gap-2  ">
+      <div className="flex  item-center justify-between mb-2">
+        {label && (
+          <label className="font-bold" htmlFor={label}>
+            {label}
+          </label>
+        )}
+        {type.toLowerCase() == "password" && (
+          <label className="text-blue-600">
+            <a href="">Forget Password?</a>
+          </label>
+        )}
+      </div>
+      <input
+        placeholder={placeholder}
+        id={label}
+        type={type}
+        onChange={onChange}
+        className={`bg-[rgb(29,29,29)] h-10 px-3 py-2 text-white border-white border-[1px] rounded focus-visible:outline-none focus-visible:outline-[rgb(223,224,222)] ${className}`}
+      />
+    </div>
   );
 }
 
