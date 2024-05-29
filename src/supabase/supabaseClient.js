5d73ce0c717b0471ac4b263261b72d40a3b2ae2a
Sudhanshu <sudhanshuDongre24@gmail.com>
2024-05-29 17:58:33 +0530

    supabase update

--- src/supabase/supabaseClient.js
+++ src/supabase/supabaseClient.js
@@ -7,12 +7,26 @@ export class Supabase {
     this.supabase = this.client(conf.supaBaseUrl, conf.supaBaseAnonKey);
   }
 
-  add() {
-    console.log(this.supabase);
+  async getPokemonData() {
+    try {
+      return await this.supabase.from("PokemonData").select("*");
+    } catch (error) {
+      console.log("Supabase Service :: getPokemonData :: error ", error);
+    }
+  }
+
+  async updatePokemonData(id, pokemonData) {
+    try {
+      await this.supabase
+        .from("PokemonData")
+        .update({ data: pokemonData })
+        .eq("id", id)
+        .select();
+    } catch (error) {
+      console.log("Supabase Service :: updatePokemon :: error ", error);
+    }
   }
 }
 
 const supabaseService = new Supabase();
-supabaseService.add();
-
 export default supabaseService;
