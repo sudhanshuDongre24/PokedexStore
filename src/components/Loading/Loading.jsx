ee2a2e0a0411cb0e5a4ec20b6d1ecc4df2722caf
Sudhanshu <sudhanshuDongre24@gmail.com>
2024-07-12 21:59:44 +0530

    Add timer for loading function

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Loading() {
+  const [timer, setTimer] = useState(false);
+
+  setTimeout(() => {
+    setTimer(true);
+  }, 10000);
+
+  console.log(timer);
+
   return (
     <div className="w-full min-h-screen flex items-center justify-center flex-col gap-0">
       <img src="/loader.svg" alt="" className=" w-[200px] mt-[-250px]" />
       <p className="mt-10">Loading....</p>
+      {timer && (
+        <div className="mt-5">
+          <h3 className="font-semibold text-xl">
+            Error Connecting Servers try Again Later.....
+          </h3>
+        </div>
+      )}
     </div>
   );
 }
